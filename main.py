27055cc5c0c65fbbf885b18ef0be343f6d132be5
caroline-betbeze <90202031+caroline-betbeze@users.noreply.github.com>
2024-08-01 10:21:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def square(x):
     """
-    Square function
+    Square function.
+    
+    @param x: Numerical value
+    @return: Square x
     """
     return x**2
 
 if __name__ == '__main__':
-    x = 7
-    square(x)
+    X = 7
+    square(X)

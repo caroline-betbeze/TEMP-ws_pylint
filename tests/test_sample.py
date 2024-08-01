47a10d749115716dd13b31a6d9fdf9f2ad5e835e
caroline-betbeze <90202031+caroline-betbeze@users.noreply.github.com>
2024-08-01 10:50:27 +0200

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -10,9 +10,21 @@ def square(x):
     @return: Square x
     """
     y = x**2
-    assert y >= 0
     return y
+
+def test_pandas(x):
+    """
+    Create df.
+    
+    @param x: Numerical value
+    @return: Square x
+    """
+    y = square(X)
+    assert y >= 0
+    df = pd.DataFrame({'x': [x], 'y': [y]})
+    return df
 
 if __name__ == '__main__':
     X = 7
-    square(X)
+    df = test_pandas(X)
+    

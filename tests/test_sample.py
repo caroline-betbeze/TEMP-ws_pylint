b27fe162aff490f1cf261e52ceebbb37f571e4a3
caroline-betbeze <90202031+caroline-betbeze@users.noreply.github.com>
2024-08-01 10:34:03 +0200

    Create test_sample.py

---
+++ tests/test_sample.py
@@ -0,0 +1,16 @@
+"Blablabla mon module"
+
+def square(x):
+    """
+    Square function.
+    
+    @param x: Numerical value
+    @return: Square x
+    """
+    y = x**2
+    assert y >= 0
+    return y
+
+if __name__ == '__main__':
+    X = 7
+    square(X)
